b822b01fb34ccafabd1fcd8628846b0941485d62
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2022-05-18 12:50:52 +0100

    First merged results across thresholds

--- code/combining_thresholds.R
+++ code/combining_thresholds.R
@@ -1,7 +1,23 @@
 # Script to combine results from various thresholds
-comb_array <- array(dim = c(101, 6253, 20))
 
-for (i in 1:20) {
-    comb_array[, , 1] <- 
-    read.csv(paste("../results/ordered_prior_score_", i, "mya.csv"))
-}
+# Load in necessary libraries
+library(dplyr)
+
+# Load in data
+file_list <- list.files("../results/mammal_thresholds/", pattern = "*.csv")
+thresh_results <- lapply(paste("../results/mammal_thresholds/",
+                    file_list, sep = ""), read.csv, row.names = 1)
+
+thresh <- array(unlist(thresh_results), dim = c(6253, 101, 20))
+
+# List species names
+species <- rownames(thresh_results[[1]])
+
+# Average results across thresholds
+scores <- thresh[, 101, ]
+
+means <- apply(scores, 1, mean)
+
+results <- data.frame(species, means)
+
+ordered <- arrange(results, desc(means))
