d76ebd49ebba376a196f0f6a0e5d155b4b351f76
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2022-07-08 16:42:08 +0100

    Finalising distance calculation code

--- code/distance_calculations.R
+++ code/distance_calculations.R
@@ -7,5 +7,20 @@ dist <- matrix(nrow = 6253, ncol = 6253)
 
 for (i in seq_len(100)) {
     dist <- ape::cophenetic.phylo(phy.block.1000[[i]])
-    
+    write.csv(dist, 
+        file = paste("../data/mammal_distances/dist_", i, ".csv", sep = ""))
 }
+
+rm(list = ls())
+# birds next
+load("../data/cleaned_bird_trees_2020.RData")
+
+dist <- matrix(nrow = 10988, ncol = 10988)
+
+for (i in seq_len(100)) {
+    dist <- ape::cophenetic.phylo(bird.trees[[i]])
+    write.csv(dist,
+        file = paste("../data/bird_distances/dist_", i, ".csv", sep = ""))
+}
+
+rm(list = ls())
