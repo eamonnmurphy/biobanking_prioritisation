cb7e1c0af5ca83a3a4950a863038e851bd0b425f
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2022-07-08 17:01:12 +0100

    Updating file locations for HPC

--- code/distance_calculations.R
+++ code/distance_calculations.R
@@ -1,26 +1,28 @@
 # Script to calculate all of the phylogenetic distances and store results
 
 # Mammals first
-load("../data/cleaned_mammal_trees_2020.RData")
+load("$HOME/biobanking_prioritisation/data/cleaned_mammal_trees_2020.RData")
 
 dist <- matrix(nrow = 6253, ncol = 6253)
 
 for (i in seq_len(100)) {
     dist <- ape::cophenetic.phylo(phy.block.1000[[i]])
-    write.csv(dist, 
-        file = paste("../data/mammal_distances/dist_", i, ".csv", sep = ""))
+    write.csv(dist, file = paste(
+        "$HOME/biobanking_prioritisation/data/mammal_distances/dist_", 
+        i, ".csv", sep = ""))
 }
 
 rm(list = ls())
 # birds next
-load("../data/cleaned_bird_trees_2020.RData")
+load("$HOME/biobanking_prioritisation/data/cleaned_bird_trees_2020.RData")
 
 dist <- matrix(nrow = 10988, ncol = 10988)
 
 for (i in seq_len(100)) {
     dist <- ape::cophenetic.phylo(bird.trees[[i]])
-    write.csv(dist,
-        file = paste("../data/bird_distances/dist_", i, ".csv", sep = ""))
+    write.csv(dist,file = paste(
+        "$HOME/biobanking_prioritisation/data/bird_distances/dist_", 
+        i, ".csv", sep = ""))
 }
 
 rm(list = ls())
