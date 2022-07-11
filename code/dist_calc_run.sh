fc1c4d985e0e5204493a5d64c1a5dc4da99f11b4
eamonnmurphy <em7657@gmail.com>
2022-07-11 15:31:03 +0100

    Final distance calculation scripts

--- code/dist_calc_run.sh
+++ code/dist_calc_run.sh
@@ -5,7 +5,10 @@
 #PBS -l select=1:ncpus=1:mem=32gb
 module load anaconda3/personal
 source activate Renv_prior
+cp $HOME/biobanking_prioritisation/data/cleaned* .
 echo "R is about to run"
 R --vanilla < $HOME/biobanking_prioritisation/code/distance_calculations.R
 echo "R has finished running"
+mv bird* $HOME/biobanking_prioritisation/data/bird_distances/
+mv mammal* $HOME/biobanking_prioritisation/data/mammal_distances/
 echo $SECONDS
