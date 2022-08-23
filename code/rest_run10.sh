6b0a6acc79d71aca9753bca948160668882d3cd5
eamonnmurphy <em7657@gmail.com>
2022-08-23 13:11:41 +0100

    Rerun of restoration sims

---
+++ code/rest_run10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Desc: Shell for running R script on HPC cluster
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=32gb
+module load anaconda3/personal
+source activate Renv_prior
+cp $HOME/biobanking_prioritisation/data/aggregated_mammal_10mya.csv .
+cp $HOME/biobanking_prioritisation/data/aggregated_bird_10mya.csv .
+cp $HOME/biobanking_prioritisation/data/mammals_50_likelihoods.csv .
+cp $HOME/biobanking_prioritisation/data/birds_50_likelihoods.csv .
+cp $HOME/biobanking_prioritisation/results/mammal_thresholds/ordered_prior_score_10mya.csv .
+cp $HOME/biobanking_prioritisation/results/25_may_bird_thresholds/50_score_10mya.csv .
+cp $HOME/biobanking_prioritisation/code/extsim_functions.R .
+echo "R is about to run"
+R --vanilla < $HOME/biobanking_prioritisation/code/restoration_sim10.R
+echo "R has finished running"
+mv *_restoration_scores.csv $HOME/biobanking_prioritisation/results/sim_10/
+echo $MINUTES
---
+++ code/restoration_sim10.R
@@ -0,0 +1,56 @@
+# # Script to calculate remaining species under extinction scenarios
+source("extsim_functions.R")
+set.seed(123)
+sims <- 10
+max_biobanked <- 1000
+thresh <- 10
+iter <- as.numeric(Sys.getenv("PBS_ARRAY_INDEX"))
+
+# Check which of four scenarios to calculate
+if (iter == 1) {
+    taxa <- "bird"
+    type <- "random"
+} else if (iter == 2) {
+    taxa <- "bird"
+    type <- "optimised"
+} else if (iter == 3) {
+    taxa <- "mammal"
+    type <- "random"
+} else if (iter == 4) {
+    taxa <- "mammal"
+    type <- "optimised"
+}
+
+# Load in the likelihood table
+like <- read.csv(paste(taxa, "s_50_likelihoods.csv", sep = ""))
+
+base_res <- run_sim(like, sims)
+
+base_avg <- average_calc(base_res)
+
+base_extinctions <- nrow(like) - sum(base_avg$survival)
+
+print(paste(taxa, ":", base_extinctions))
+
+# quick test of proper restoration check
+if (taxa == "bird") {
+    priorities <- priority_builder(paste("50_score_", thresh, "mya.csv",
+        sep = ""))
+} else if (taxa == "mammal") {
+    priorities <- priority_builder(paste("ordered_prior_score_", thresh,
+        "mya.csv", sep = ""))
+}
+
+bb <- seq(from = 0, to = max_biobanked, length.out = 101)
+restoration <- matrix(nrow = sims, ncol = length(bb))
+if (type == "random") {
+    restoration <- sapply(bb, random_restoration_prioritised,
+        sim_res = base_res, taxa = taxa, sims = sims, thresh = thresh)
+} else if (type == "optimised") {
+    restoration <- sapply(bb, optimised_restoration_prioritised,
+        sim_res = base_res, priorities = priorities, taxa = taxa, sims = sims,
+        thresh = thresh)
+}
+
+write.csv(restoration, file = paste(taxa, type, "restoration_scores.csv",
+    sep = "_"), row.names = FALSE)
