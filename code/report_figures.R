a7917a80f4d65bdf6775ee24719d871cd528ab54
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2022-08-24 11:00:10 +0100

    Fixing figure formatting

--- code/report_figures.R
+++ code/report_figures.R
@@ -14,6 +14,21 @@ like_bird_500 <- read.csv("../data/birds_50_likelihoods.csv", header = TRUE)
 like_bird_pessimistic <- read.csv("../data/birds_pessimistic_likelihoods.csv",
     header = TRUE)
 
+rs_bird_50 <- read.csv("../results/related_survival/bird_50_rs_10mya.csv",
+    row.names = 1)
+rs_bird_500 <- read.csv("../results/related_survival/bird_500_rs_10mya.csv",
+    row.names = 1)
+rs_bird_pessimistic <- read.csv(
+    "../results/related_survival/bird_pessimistic_rs_10mya.csv",
+    row.names = 1)
+rs_mammal_50 <- read.csv("../results/related_survival/mammal_50_rs_10mya.csv",
+    row.names = 1)
+rs_mammal_500 <- read.csv("../results/related_survival/mammal_500_rs_10mya.csv",
+    row.names = 1)
+rs_mammal_pessimistic <- read.csv(
+    "../results/related_survival/mammal_pessimistic_rs_10mya.csv",
+    row.names = 1)
+
 plot_ext_like <- function(taxa, model) {
     values <- get(paste("like_", taxa, "_", model, sep = ""))
     if (model == "pessimistic") {
@@ -31,10 +46,9 @@ plot_ps <- function(taxa, model, threshold = 10) {
     values <- read.csv(paste("../results/", taxa, "_ps/", model,
         "_score_", threshold, "mya.csv", sep = ""))
     if (model == "pessimistic") {
-        name <- paste("Prioritisation scores, ", model, " model", sep = "")
+        name <- "Pessimistic model"
     } else {
-        name <- paste("Prioritisation scores, ", model, " year model",
-            sep = "")
+        name <- paste("IUCN", model, "year model", sep = " ")
     }
     if (taxa == "mammal") {
         ys <- c(0, 6000)
@@ -42,85 +56,123 @@ plot_ps <- function(taxa, model, threshold = 10) {
         ys <- c(0, 10000)
     }
 
-    hist(values[, 101], xlab = "Priority score", breaks = 20,
+    hist(values[, 101], xlab = "Priority Score", breaks = 20,
         ylab = "Number of species", main = "",  xlim = c(0, 1), ylim = ys)
+}
+
+plot_rs <- function(taxa, model) {
+    values <- get(paste("rs", taxa, model, sep = "_"))
+    if (taxa == "mammal") {
+        ys <- c(0, 6000)
+    } else {
+        ys <- c(0, 7000)
+    }
+
+    hist(values$average, xlab = "Probability of compatible relative surviving",
+        breaks = 20, ylab = "Number of species", main = NULL,
+        ylim = ys)
 }
 
 taxa <- c("mammal", "bird")
 model <- c("50", "500", "pessimistic")
 
-pdf(file = "../results/combined_hist.pdf")
-par(mfrow = c(2, 3))
-letter <- c("A")
 for (i in seq_along(taxa)) {
+    pdf(file = paste("../results/", taxa[i], "_combined_hist.pdf", sep = ""),
+        pointsize = 11)
+    par(mfcol = c(2, 3), mar = c(5.1, 4.1, 6.1, 1.1))
     for (j in seq_along(model)) {
         plot_ps(taxa[i], model[j])
-        if (i == 1) {
-            if (model[j] == "pessimistic") {
-                mtext("Pessimistic model")
-            } else {
-                mtext(paste("IUCN", model[j], "year model", sep = " "))
-            }
+        if (model[j] == "pessimistic") {
+            mtext("Pessimistic model")
+        } else {
+            mtext(paste("IUCN", model[j], "year model", sep = " "))
         }
-        if (i == 1 & j == 2) {
-            mtext("Mammals", line = 2, cex = 1.5)
-        }
-        if (i == 2 & j == 2) {
-            mtext("Birds", line = 2, cex = 1.5)
-        }
+        # if (i == 1 & j == 2) {
+        #     mtext("Mammals", line = 2, cex = 1.5)
+        # }
+        # if (i == 2 & j == 2) {
+        #     mtext("Birds", line = 2, cex = 1.5)
+        # }
         pplt <- par("plt")
         adjx <- (0 - pplt[1]) / (pplt[2] - pplt[1])
-        liney <- par("mar")[3] - 2.5
-        if (i == 1) {
+        liney <- 1
+        # if (i == 1) {
             mtext(LETTERS[j], adj = adjx, line = liney)
-        } else if (i == 2) {
-            mtext(LETTERS[j + 3], adj = adjx, line = liney)
+        # } else if (i == 2) {
+        #     mtext(LETTERS[j + 3], adj = adjx, line = liney)
+        # }
+        plot_rs(taxa[i], model[j])
+        if (model[j] == "pessimistic") {
+            mtext("Pessimistic model")
+        } else {
+            mtext(paste("IUCN", model[j], "year model", sep = " "))
         }
+
+        pplt <- par("plt")
+        adjx <- (0 - pplt[1]) / (pplt[2] - pplt[1])
+        liney <- 1
+
+        mtext(LETTERS[j + 3], adj = adjx, line = liney)
+
     }
+    graphics.off()
 }
-graphics.off()
+
+##### Figure for related survival histograms ####
+
+
+par(mfcol = c(2, 3), mar = c(5.1, 4.1, 6.1, 1.1))
+hist(rs_mammal_50$average, ylim = c(0, 6000), breaks = 20)
+hist(rs_mammal_500$average, ylim = c(0, 6000), breaks = 20)
+hist(rs_mammal_pessimistic$average, ylim = c(0, 6000), breaks = 20)
+
+hist(rs_bird_50$average, ylim = c(0, 7000), breaks = 20)
+hist(rs_bird_500$average, ylim = c(0, 7000), breaks = 20)
+hist(rs_bird_pessimistic$average, ylim = c(0, 7000), breaks = 20)
 
 #### Varying thresholds visualisation ####
 bird_table <- read.csv("../results/bird_threshold_table.csv", row.names = 1)
 mammal_table <- read.csv("../results/mammal_threshold_table.csv", row.names = 1)
-pdf("../results/threshold_viz.pdf")
+pdf("../results/threshold_viz.pdf", height = 9, width = 6, pointsize = 11)
 par(mfcol = c(2, 1))
 
 # Plot the mammal table
 plot(mammal_table$fifty_year, type = "l", xlab = "Threshold for MRCA (mya)",
-    ylim = c(0, 1000), ylab = "No. of species", col = "blue")
+    ylim = c(0, 701), ylab = "No. of species", col = "blue")
 
 lines(mammal_table$fivehundred_year, col = "#6c3092")
 lines(mammal_table$pessimistic, col = "red")
 
 abline(h = 199, col = "black", lty = 2)
 text(x = 10, y = 239, "Critically endangered mammal species")
-abline(h = 701, col = "black", lty = 2)
-text(x = 10, y = 741, "Endangered and critically endangered mammal species")
 
-legend(x = "topright", legend = c("IUCN 50 year model",
+legend(x = 11, y = 600, legend = c("IUCN 50 year model",
     "IUCN 500 year model", "Pessimistic scenario"), col = c("blue", "purple",
-        "red"), lty = 1)
+        "red"), lty = 1, box.col = "white")
 
-mtext("A: Mammals", line = 1)
+pplt <- par("plt")
+adjx <- (0 - pplt[1]) / (pplt[2] - pplt[1])
+liney <- 1
+mtext("A", adj = adjx, line = liney, cex = 1.2)
 
 # Plot the bird results
 plot(bird_table$fifty_year, type = "l", xlab = "Threshold for MRCA (mya)",
-    ylim = c(0, 1000), ylab = "No. of species", col = "blue")
+    ylim = c(0, 691), ylab = "No. of species", col = "blue")
 
 lines(bird_table$fivehundred_year, col = "#6c3092")
 lines(bird_table$pessimistic, col = "red")
 
 abline(h = 230, col = "black", lty = 2)
 text(x = 10, y = 270, "Critically endangered bird species")
-abline(h = 691, col = "black", lty = 2)
-text(x = 10, y = 731, "Endangered and critically endangered bird species")
 
-legend(x = "topright", legend = c("IUCN 50 year model",
+legend(x = 11, y = 600, legend = c("IUCN 50 year model",
     "IUCN 500 year model", "Pessimistic scenario"), col = c("blue", "purple",
-        "red"), lty = 1)
+        "red"), lty = 1, box.col = "white")
 
-mtext("B: Birds", line = 1)
+pplt <- par("plt")
+adjx <- (0 - pplt[1]) / (pplt[2] - pplt[1])
+liney <- 1
+mtext("B", adj = adjx, line = liney, cex = 1.2)
 
 graphics.off()
 
@@ -131,11 +183,15 @@ mammal_random_rest <-
     read.csv("../results/mammal_random_restoration_scores.csv")
 mammal_optim_rest <-
     read.csv("../results/mammal_optimised_restoration_scores.csv")
+mammal_cat_rest <-
+    read.csv("../results/cat_2/mammal_cat_restoration_scores.csv")
 
 bird_random_rest <-
     read.csv("../results/bird_random_restoration_scores.csv")
 bird_optim_rest <-
     read.csv("../results/bird_optimised_restoration_scores.csv")
+bird_cat_rest <-
+    read.csv("../results/cat_2/bird_cat_restoration_scores.csv")
 
 pdf("../results/restoration.pdf")
 
@@ -150,6 +206,9 @@ points(seq(0, 1000, by = 10), mammal_random_rest[2, ])
 points(seq(0, 1000, by = 10), mammal_optim_rest[1, ], col = "red")
 points(seq(0, 1000, by = 10), mammal_optim_rest[2, ], col = "red")
 
+points(seq(0, 1000, by = 10), mammal_cat_rest[1, ], col = "blue")
+points(seq(0, 1000, by = 10), mammal_cat_rest[2, ], col = "blue")
+
 legend(x = "topright", legend = c("Random biobanking", "Optimised biobanking"),
     col = c("black", "red"), pch = 1)
 
@@ -161,6 +220,9 @@ points(seq(0, 1000, by = 10), bird_random_rest[2, ])
 
 points(seq(0, 1000, by = 10), bird_optim_rest[1, ], col = "red")
 points(seq(0, 1000, by = 10), bird_optim_rest[2, ], col = "red")
+
+points(seq(0, 1000, by = 10), bird_cat_rest[1, ], col = "blue")
+points(seq(0, 1000, by = 10), bird_cat_rest[2, ], col = "blue")
 
 legend(x = "topright", legend = c("Random biobanking", "Optimised biobanking"),
     col = c("black", "red"), pch = 1)
